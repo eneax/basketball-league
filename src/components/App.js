48a88ca4a1a61c94a58346e1e08f92d32835094d
eneax <19818937+eneax@users.noreply.github.com>
2018-06-11 16:21:22 +0200

    Config 404 route

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Navbar from './Navbar';
 import Players from './Players';
@@ -11,9 +11,13 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
-          <Route exact path='/' component={Home}>Home</Route>
-          <Route path='/players' component={Players}>Players</Route>
-          <Route path='/teams' component={Teams}>Teams</Route>
+
+          <Switch>
+            <Route exact path='/' component={Home}>Home</Route>
+            <Route path='/players' component={Players}>Players</Route>
+            <Route path='/teams' component={Teams}>Teams</Route>
+            <Route render={() => <h1 className='text-center'>404</h1>}></Route>
+          </Switch>
         </div>
       </Router>
     );
