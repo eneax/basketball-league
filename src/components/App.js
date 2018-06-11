e2075217ebe8c3bf59d855ba310e4e85e56cbddf
eneax <19818937+eneax@users.noreply.github.com>
2018-06-11 13:50:08 +0200

    Config initial routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Navbar from './Navbar';
+import Players from './Players';
+import Teams from './Teams';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        React Router v4
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path='/' component={Home}>Home</Route>
+          <Route path='/players' component={Players}>Players</Route>
+          <Route path='/teams' component={Teams}>Teams</Route>
+        </div>
+      </Router>
     );
   }
 }
