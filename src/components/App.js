ddc4f31f85f1463d49d31f18b7416f9da7cd8bb1
eneax <19818937+eneax@users.noreply.github.com>
2018-08-08 18:26:05 +0200

    Set up the team's page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Players from './Players';
 import Teams from './Teams';
+import TeamPage from './TeamPage';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route path='/players' component={Players} />
             <Route path='/teams' component={Teams} />
+            <Route exact path='/:teamId' component={TeamPage} />
             <Route render={() => <h1 className='text-center'>404</h1>} />
           </Switch>
         </div>
